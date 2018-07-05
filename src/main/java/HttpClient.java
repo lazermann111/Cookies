fe4505a5484c067cfc76b46ebca09e61a0ecd067
lazermann111 <lazermann111@gmail.com>
2018-07-06 00:46:18 +0300

    init

---
+++ src/main/java/HttpClient.java
@@ -0,0 +1,61 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.mashape.unirest.http.ObjectMapper;
+import com.mashape.unirest.http.Unirest;
+
+import java.io.IOException;
+
+public class HttpClient
+{
+
+    public static String BASE_URL = "localhost:8001";
+
+
+    static
+    {
+        Unirest.setConcurrency(10,10);
+
+
+        com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+        jacksonObjectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        Unirest.setObjectMapper(new ObjectMapper() {
+            public <T> T readValue(String value, Class<T> valueType) {
+                try {
+                    return jacksonObjectMapper.readValue(value, valueType);
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            public String writeValue(Object value) {
+                try {
+                    return jacksonObjectMapper.writeValueAsString(value);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+
+    public static void addNewCookie(CookieInfoDto cookieInfoDto)
+    {
+
+        try
+        {
+            Unirest.get(BASE_URL + "/addCookie")
+                    .routeParam("proxy", cookieInfoDto.getProxy())
+                    .routeParam("cookie", cookieInfoDto.getCookie());
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            AppiumLogger.log("addNewCookie exception: " + e.getMessage());
+        }
+
+
+    }
+}
